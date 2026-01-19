50eb86fd912012916410c3d22e20884571a92559
MaabhagawatiRKengineers <maabhagawatirkengineers@gmail.com>
2026-01-19 22:59:59 +0530

    Create SEO.tsx

---
+++ src/components/SEO.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+const SEO: React.FC = () => {
+  return (
+    <Helmet>
+      <title>BALSAN | Business & Finance Solutions</title>
+      <meta
+        name="description"
+        content="BALSAN provides accounting, taxation, GST, compliance, financial planning and business solutions for growing companies."
+      />
+      <meta
+        name="keywords"
+        content="Accounting, GST, Income Tax, Business Compliance, Finance Solutions, Business consultancy"
+      />
+    </Helmet>
+  );
+};
+
+
+export default SEO;
